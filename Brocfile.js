9f92feef08c675045cf5032eb40beab1a3412399
Andy Pickler <andy.pickler@gmail.com>
2015-05-11 13:13:28 -0600

    Add borrowers-dates helper

--- Brocfile.js
+++ Brocfile.js
@@ -35,5 +35,12 @@ app.import('bower_components/picnic/releases/plugins.min.css');
 app.import('bower_components/picnic/releases/picnic.min.css');
 
 app.import('bower_components/moment/moment.js');
+app.import('bower_components/borrowers-dates/index.js', {
+  exports: {
+    'borrowers-dates': [
+      'format'
+    ]
+  }
+});
 
 module.exports = app.toTree();
